deb70ec75345a2f72264cb1ebd7148a63dfe6c07
engrpaim <joseephraimbatacan@gmail.com>
2026-09-04 07:54:45 +0800

    rm sqlite

--- database/migrations/2026_08_01_085245_add_quantity_column_order_models.php
+++ database/migrations/2026_08_01_085245_add_quantity_column_order_models.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-       Schema::table('order_models', function(Blueprint $table){
+       Schema::table('Order_Models', function(Blueprint $table){
                 $table->double('Quantity')->nullable();
                 $table->string('Model_Code')->nullable();
            });
